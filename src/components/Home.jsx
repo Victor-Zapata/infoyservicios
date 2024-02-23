b6a630840d54a64527a8c754cd6035738919e923
Valentin <valentinaless07@hotmail.com>
2024-02-23 15:42:03 -0300

    added home page styles

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,28 +2,28 @@ import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
-    <div>
-      <h1 style={{ margin: '10px 0' }}>Info y servicios</h1>
+    <div style={{padding: '25px'}}>
+      <h1 style={{ margin: '15px 0', color: 'rgb(20, 20, 112)'}}>Info y servicios</h1>
 
       <div className="grid-container">
         <div className="grid-item">
           <Link to="/dolares" style={{ color: 'white' }}>
-            <h2>Cotizaciones</h2>
+            <h2>💵 Cotizaciones</h2>
           </Link>
         </div>
         <div className="grid-item">
           <Link to="/funcionarios" style={{ color: 'white', height: '100%' }}>
-            <h2>Funcionarios</h2>
+            <h2>🏢 Funcionarios</h2>
           </Link>
         </div>
         <div className="grid-item">
           <Link to="/movies" style={{ color: 'white', height: '100%' }}>
-            <h2>Cine</h2>
+            <h2>🎬 Cine</h2>
           </Link>
         </div>
         <div className="grid-item">
           <Link to="/deportes" style={{ color: 'white', height: '100%' }}>
-            <h2>Deportes</h2>
+            <h2>⚽ Deportes</h2>
           </Link>
         </div>
       </div>
